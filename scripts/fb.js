ec46a996a4597fff3c3fd77607b2ee91eb9b965f
sivasethusaravanans <sivasethu@wowtalkies.com>
2023-10-26 17:32:47 +0530

    first commit

---
+++ scripts/fb.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('./fbAuth/fb.json');
+
+
+
+ 
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sss-005-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+const app = express();
+const port = 3000;
+
+// Define a route to update the "Master/Timestamp" node
+app.get('/updateTimestamp', (req, res) => {
+  const databaseRef = admin.database().ref('Master/Timestamp');
+  
+  // Update the timestamp to a new value (e.g., the current time).
+  databaseRef.set(new Date().toISOString(), (error) => {
+    if (error) {
+      res.status(500).send('Error updating timestamp.');
+    } else {
+      res.status(200).send('Timestamp updated successfully.');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
