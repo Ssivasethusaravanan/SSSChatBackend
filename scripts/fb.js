2fd242aecb7a750c12cf3342cc5f8f6c98c93a1b
sivasethusaravanans <sivasethu@wowtalkies.com>
2023-10-27 17:02:59 +0530

    token transfer fun added

--- scripts/fb.js
+++ scripts/fb.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const path = require('path');
+const bodyParser = require('body-parser'); // Add body-parser middleware
+const { transferTokens } = require('./transfer');
 const serviceAccount = require(path.join(__dirname, 'fb.json'));
-
- 
- 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,9 +13,12 @@ admin.initializeApp({
 const app = express();
 const port = 3000;
 
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
 app.get('/updateTimestamp', (req, res) => {
   const databaseRef = admin.database().ref('Master/Timestamp');
-  
+
   // Update the timestamp to a new value (e.g., the current time).
   databaseRef.set(new Date().toISOString(), (error) => {
     if (error) {
@@ -25,6 +27,17 @@ app.get('/updateTimestamp', (req, res) => {
       res.status(200).send('Timestamp updated successfully.');
     }
   });
+});
+
+app.post('/transferToken', async (req, res) => {
+  const { toaddress } = req.body; // Extract the recipient address from the request body
+
+  try {
+    await transferTokens(toaddress, res);
+  } catch (error) {
+    console.error("Error transferring tokens:", error);
+    res.status(500).send(`Error transferring tokens: ${error.message}`);
+  }
 });
 
 app.listen(port, () => {
