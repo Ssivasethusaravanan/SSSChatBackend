8828d95d86c493ff472e1e06073c129f4a2b0dd7
sivasethusaravanans <sivasethu@wowtalkies.com>
2023-11-16 18:00:27 +0530

    getbalances added

---
+++ scripts/balances.js
@@ -0,0 +1,46 @@
+
+const { Alchemy, Network }=require( "alchemy-sdk");
+
+const config = {
+  apiKey: "JVLV4xu3CjSUKUNEfXXJSuJ3xz6ZrCLe",
+  network: Network.MATIC_MUMBAI,
+};
+const alchemy = new Alchemy(config);
+
+const getBalances = async (ownerAddress) => {
+  // Wallet address
+  const address = ownerAddress;
+
+  // Get token balances
+  const balances = await alchemy.core.getTokenBalances(address);
+
+  // Remove tokens with zero balance
+  const nonZeroBalances = balances.tokenBalances.filter((token) => {
+    return token.tokenBalance !== "0";
+  });
+
+  console.log(`Token balances of ${address} \n`);
+
+  // Counter for SNo of final output
+  let i = 1;
+
+  // Loop through all tokens with non-zero balance
+  for (let token of nonZeroBalances) {
+    // Get balance of token
+    let balance = token.tokenBalance;
+
+    // Get metadata of token
+    const metadata = await alchemy.core.getTokenMetadata(token.contractAddress);
+
+    // Compute token balance in human-readable format
+    balance = balance / Math.pow(10, metadata.decimals);
+    balance = balance.toFixed(2);
+
+    // Print name, balance, and symbol of token
+    console.log(`${i++}. ${metadata.name}: ${balance} ${metadata.symbol}`);
+  }
+};
+
+ 
+  
+module.exports.getBalances = getBalances;
